55e72b4a5087ed330883cedae7919531419e21d0
Se7enb2st <ethanhe1992@gmail.com>
2016-06-12 16:27:01 +0000

    Add strikethrough for completed items

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,25 +1,28 @@
-$(function() {
-
+$(function () {
   // The taskHtml method takes in a JavaScript representation
   // of the task and produces an HTML representation using
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
     " data-id='" + task.id + "'" +
     checkedStatus +
     '><label>' +
     task.title +
     '</label></div></li>';
+
     return liElement;
   }
+
 
   // toggleTask takes in an HTML representation of the
   // an event that fires from an HTML representation of
   // the toggle checkbox and  performs an API request to toggle
   // the value of the `done` field
   function toggleTask(e) {
-    var itemId = $(e.target).data("id");
+    var itemId = $(e.target).data('id' );
 
     var doneValue = Boolean($(e.target).is(':checked'));
 
@@ -28,7 +31,12 @@ $(function() {
       task: {
         done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+    } );
   }
 
   $.get("/tasks").success( function( data ) {
